da773b1cca150fab21b67da65c73fe2f74520d9c
Matt Millican <matt@mattmillican.com>
2020-10-30 00:59:59 -0500

    Change app.vue to js from ts to match other components

--- src/App.vue
+++ src/App.vue
@@ -4,41 +4,39 @@
 
     <div class="container body">
       <router-view />
-
-      <!-- <b-toast title="BootstrapVue" static visible variant="success">
-        Hello, world! This is a toast message.
-      </b-toast> -->
     </div>
 
     <!-- <loading-indicator /> -->
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue, Watch } from 'vue-property-decorator';
+<script>
 import MainNav from '@/components/MainNav.vue';
 
-@Component({
+export default {
   components: {
     MainNav
-  }
-})
-export default class App extends Vue {
-  protected hideNavBar = false;
-
-  protected async created() {
-    await this.configureApp();
-  }
-
-  @Watch('$route', { immediate: true })
-  protected async configureApp() {
-    this.hideNavBar = this.$route?.meta?.hideNavBar ?? false;
-
-    if (await this.$auth.isAuthenticated()) {
-      await this.$store.dispatch('setCurrentUser');
+  },
+  data() {
+    return {
+      hideNavBar: false
+    };
+  },
+  async created() {
+    await this.configureApp()
+  },
+  watch: {
+    $route: 'configureApp'
+  },
+  methods: {
+    async configureApp() {
+      this.hideNavBar = this.$route?.meta?.hideNavBar ?? false;
+      if (await this.$auth.isAuthenticated()) {
+        await this.$store.dispatch('setCurrentUser');
+      }
     }
   }
-}
+};
 </script>
 
 <style lang="scss">
